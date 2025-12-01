da25a14476d82b298890aa4e279b31eebbabcf19
Jordi Rubio Arbona <131033900+jordirubio01@users.noreply.github.com>
2025-12-01 23:03:38 +0100

    S1 7th Commit
    Minor changes:
    - All comments are now in English.
    - Prints modified for better visualisation.

--- 01-S1/first_seminar.py
+++ 01-S1/first_seminar.py
@@ -3,7 +3,8 @@ import os
 from PIL import Image
 import numpy as np
 import unittest
-# 1st TASK
+
+## 2nd TASK
 class ColorTranslator:
     # Convert from RGB scale to YUV
     def RGBtoYUV(self, R, G, B):
@@ -19,6 +20,9 @@ class ColorTranslator:
         B = 1.164 * (Y - 16) + 2.018 * (U - 128)
         return R, G, B
 
+print("\n\n========================================")
+print("2nd Task")
+print("========================================")
 converter = ColorTranslator()
 R=194
 G=61
@@ -29,22 +33,24 @@ print(f"RGB: {R}, {G}, {B}; YUV: {Y}, {U}, {V}.")
 R, G, B = converter.YUVtoRGB(Y, U, V)
 print(f"YUV: {Y}, {U}, {V}; RGB: {R}, {G}, {B}.")
 
-#2nd TASK
-#Foto from Halyna Chemerys https://unsplash.com/es/fotos/un-grupo-de-caballos-u3LdAMV_CIo?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+
+## 3rd TASK
+# Foto from Halyna Chemerys https://unsplash.com/es/fotos/un-grupo-de-caballos-u3LdAMV_CIo?utm_source=unsplash&utm_medium=referral&utm_content=creditShareLink
+# Scale shell command --> ffmpeg -i input_image.jpg -vf scale=500:500 output_image.jpg  (vb = video filter)
 def resizeImageOS(filename, output_width, output_height):
     os.system(f"ffmpeg -i {filename}.jpg -vf scale={output_width}:{output_height} horses_{output_width}_{output_height}.jpg")
 
+print("\n\n========================================")
+print("3rd Task (using OS)")
+print("========================================")
 filename="horses"
 print(os.path.exists(filename))
 sizes = [[1000,1000], [500,500], [50,50], [100,100]]
 for size in sizes:
     resizeImageOS(filename, size[0], size[1])
-#TODO: Improve the function if possible (maybe without using the os command)
 
-#2nd TASK with ffmpeg
-#Code example found in https://github.com/kkroening/ffmpeg-python/blob/master/examples/README.md
-#Scale shell command --> ffmpeg -i input_image.jpg -vf scale=500:500 output_image.jpg  (vb = video filter)
-
+## 3rd TASK with ffmpeg
+# Code example found in https://github.com/kkroening/ffmpeg-python/blob/master/examples/README.md
 def resizeImage_ffmpeg(filename, output_width, output_height):
     output_name = f"horses_{output_width}_{output_height}_ffmpeg.jpg"
     (
@@ -55,14 +61,19 @@ def resizeImage_ffmpeg(filename, output_width, output_height):
         .overwrite_output()
         .run(quiet=True)
     )
+    print(f"New image {output_name} created")
     return output_name
 
+print("\n\n========================================")
+print("3rd Task (using ffmpeg)")
+print("========================================")
 filename="horses"
 sizes = [[1000,1000], [500,500], [50,50], [100,100]]
 for size in sizes:
     resizeImage_ffmpeg(filename, size[0], size[1])
 
-#3rd Task
+
+## 4th Task
 def serpentine(matrix):
 
     rows, cols = matrix.shape
@@ -72,78 +83,83 @@ def serpentine(matrix):
         for j in range(cols):
             sum_idx = i + j
             if sum_idx % 2 == 0:
-                # Si la suma d'índexs és parella, afegim al principi (pujada)
+                # If the indexes sum is even, add at the start (ascent)
                 solution[sum_idx].insert(0, matrix[i][j])
             else:
-                # Si és senar, afegim al final (baixada)
+                # If odd, add at the end (descent)
                 solution[sum_idx].append(matrix[i][j])
 
-    # Aplanar la llista de llistes en una sola
+    # We return the flatten list of lists in one
     return [item for sublist in solution for item in sublist]
 
-
-# Simulem llegir un bloc de 8x8 (com es fa a JPEG) podem fer servir una imatge real tenint en compte que JPEG treballa en blocs de 8x8
+# We simulate reading an 8x8 block (as in JPEG), we can use a real image as JPEG works with 8x8 blocks
 filename = "horses.jpg" 
-# Convertim a gris per simplificar-ho (valors 0-255)
+# We convert to gray to simplify (values 0-255)
 img = Image.open(filename).convert('L') 
 img_array = np.array(img)
 
-# Agafem només el primer bloc de 8x8 píxels de la imatge --> Tal com deiem a teoria: "used to convert the 8x8 DCT spectrum"
+# We take only the first 8x8 pixels block of the image (as seen in theory, it is used to convert the 8x8 DCT spectrum)
 block_8x8 = img_array[0:8, 0:8] 
 
-print("----3rd Task----")
-print("--- Bloc original 8x8 (Bytes/Pixels) ---")
+print("\n\n========================================")
+print("4th Task")
+print("========================================")
+print("--- Original 8x8 Block (Bytes/Pixels) ---")
 print(block_8x8)
 
 serpentine_bytes = serpentine(block_8x8)
 
-print("\n--- Sequencia Serpentine (Zig-Zag) ---")
+print("\n--- Serpentine Sequence (Zig-Zag) ---")
 print(serpentine_bytes)
-print(f"Total elements: {len(serpentine_bytes)}") # Hauria de ser 64 per anar bé
+print(f"Total elements: {len(serpentine_bytes)}") # It should be 64 to be right
 
-#4th Task
+## 5th Task
 def compress_bw_hardest(input_filename):
     output_name = f"bw_compressed_{input_filename}"
     try:
         (
             ffmpeg
             .input(input_filename)
-            .filter('hue', s=0)       # Filtre: Saturation = 0 (blanc i negre)
-            .output(output_name, **{'qscale:v': 31}) # qscale:v 31 és la pitjor qualitat, és a dir, màxima compressió
+            .filter('hue', s=0)       # Filtre: Saturation = 0 (black and white)
+            .output(output_name, **{'qscale:v': 31}) # qscale:v 31 is the worst quality, that is, hardest compression
             .overwrite_output()
             .run()          
         )
-        return output_name
     except ffmpeg.Error as e:
-        print("Hi ha hagut un error amb FFmpeg:", e)
+        print("There has been an error with FFmpeg:", e) # We had some problems when testing
         return
 
-    # Comparació de mides
+    # Size comparison
     if os.path.exists(output_name):
         original_size = os.path.getsize(input_filename)
         compressed_size = os.path.getsize(output_name)
         compression_ratio = (1 - (compressed_size / original_size)) * 100
 
-filename = "horses.jpg" 
-compress_bw_hardest(filename)   
+        return output_name
 
-#5th Task
+print("\n\n========================================")
+print("5th Task")
+print("========================================")
+filename = "horses.jpg"
+compress_bw_hardest(filename)
+
+## 6th Task
 def run_length_encoding(data):
-    #La funció substitueisix les seqüències de zeros per un parell (0, count).
+    # The function subtitutes the sequencies of zeros for a pair (0, count)
 
     encoded = []
     i = 0
 
     while i < len(data):
-        # Cas 1: El valor NO és zero
+        # Case 1: the value is NOT zero
         if data[i] != 0:
             encoded.append(data[i])
             i += 1
         
-        # Cas 2: El valor ÉS zero
+        # Case 2: the value IS zero
         else:
             count = 0
-            # Comptem quants zeros seguits hi ha
+            # We count how many consecutive zeros there are
             while i < len(data) and data[i] == 0:
                 count += 1
                 i += 1
@@ -154,7 +170,7 @@ def run_length_encoding(data):
     return encoded
 
 
-#Vector simulat que s'assembli al resultat d'una DCT quantitzada --> Molta informació al principi, molts zeros al final
+# Simulation vector similar to the result of a quantized DCT (a lot of information a the start, lots of zeros at the end)
 sim_vector = [
     25, 12, 10, 0, 0, 0, 0, 5,  
     0, 0, 2, 0,                 
@@ -163,17 +179,19 @@ sim_vector = [
     0, 0, 0, 0, 0, 0, 0, 0
 ]
 
+print("\n\n========================================")
+print("6th Task")
+print("========================================")
 rle_result = run_length_encoding(sim_vector)
-print("----5th Task----")
-print(f"Sortida (Encoded):    {rle_result}")
-print(f"Mida comprimida: {len(rle_result)} elements")
+print(f"Output (Encoded):    {rle_result}")
+print(f"Compressed size: {len(rle_result)} elements")
 
-# 6th Task: DCT Converter Class
+## 7th Task: DCT Converter Class
 class DCT_Converter:
     def __init__(self, size=8):
         self.N = size
         # We precompute the Transformation Matrix (T) on initialization
-        # so we don't have to recalculate cosines every time.
+        # so we don't have to recalculate cosines every time
         self.T = self._create_basis_matrix(self.N)
 
     def _create_basis_matrix(self, N):
@@ -209,38 +227,38 @@ class DCT_Converter:
         # Add 128 back
         return reconstructed + 128
 
-# --- TESTING THE CLASS ---
+# Testing the class
+print("\n\n========================================")
+print("7th Task")
+print("========================================")
 
-print("\n---- 6th Task: DCT Class Test ----")
-
-#Initialize the converter
+# Initialize the converter
 dct_machine = DCT_Converter(size=8)
 
-#Use the 'block_8x8' we extracted in Task 3 (Horses)
-# Note: Ensure block_8x8 is defined from the previous code execution
-# If running standalone, uncomment the line below:
+# Use the 'block_8x8' we extracted in Task 3 (Horses)
+# If running only this part, uncomment the line below:
 # block_8x8 = np.random.randint(0, 255, (8,8)) 
 
 print("Original Block (Top-Left 4x4 shown):")
 print(block_8x8[:4, :4])
 
-#Apply Forward DCT
+# Apply Forward DCT
 dct_coefficients = dct_machine.perform_DCT(block_8x8)
 print("\nDCT Coefficients (Top-Left 4x4 shown):")
 print("Note the DC coefficient (0,0) is usually the largest.")
 print(np.round(dct_coefficients[:4, :4], 1))
 
-#Apply Inverse DCT (IDCT)
+# Apply Inverse DCT (IDCT)
 recovered_block = dct_machine.perform_IDCT(dct_coefficients)
 print("\nReconstructed Block (Top-Left 4x4 shown):")
 print(np.round(recovered_block[:4, :4]))
 
-#Check if they are similar
+# Check if they are similar
 error = np.mean((block_8x8 - recovered_block) ** 2)
 print(f"\nMean Squared Error between original and reconstructed: {error:.5f}")
 
 
-#7th Task
+## 8th Task
 class DWT_Converter:
     
     def __init__(self):
@@ -260,7 +278,7 @@ class DWT_Converter:
         if rows % 2 != 0 or cols % 2 != 0:
             raise ValueError("Image dimensions must be even for this simple DWT.")
 
-        # --- STEP 1: Process Rows ---
+        # STEP 1: Process Rows
         # We split the image into even columns and odd columns
         # evens = columns 0, 2, 4... | odds = columns 1, 3, 5...
         evens_row = data[:, 0::2]
@@ -275,7 +293,7 @@ class DWT_Converter:
         # Combine them horizontally: [ L | H ]
         processed_rows = np.hstack((L_row, H_row))
 
-        # --- STEP 2: Process Columns ---
+        # STEP 2: Process Columns
         # Now we apply the same logic to the columns of the result
         evens_col = processed_rows[0::2, :]
         odds_col  = processed_rows[1::2, :]
@@ -304,7 +322,7 @@ class DWT_Converter:
         half_rows = rows // 2
         half_cols = cols // 2
 
-        # --- STEP 1: Inverse Columns ---
+        # STEP 1: Inverse Columns
         # Split top (Approximation) and bottom (Detail)
         # Recall we stacked them vertically in forward pass
         L_col = dwt_matrix[:half_rows, :]
@@ -321,7 +339,7 @@ class DWT_Converter:
         reconstructed_cols[0::2, :] = evens_col
         reconstructed_cols[1::2, :] = odds_col
 
-        # --- STEP 2: Inverse Rows ---
+        # STEP 2: Inverse Rows
         # Now split left and right
         L_row = reconstructed_cols[:, :half_cols]
         H_row = reconstructed_cols[:, half_cols:]
@@ -335,7 +353,9 @@ class DWT_Converter:
 
         return final_image
 
-print("\n---- 7th Task ----")
+print("\n\n========================================")
+print("8th Task")
+print("========================================")
 
 dwt_machine = DWT_Converter()
 
@@ -362,25 +382,25 @@ print(np.round(recovered_img[:4, :4]))
 error = np.mean((block_8x8 - recovered_img) ** 2)
 print(f"\nMean Squared Error: {error:.5f}")
 
-#8th Task: Unit Tests
+## 9th Task: Unit Tests
 class TestMultimediaMethods(unittest.TestCase):
 
     def setUp(self):
-        # Aquest mètode s'executa automàticament ABANS de cada test.
-        # Creem una imatge "dummy" per no dependre de si tens 'horses.jpg' o no.
+        # This method executes automatically BEFORE every test
+        # We create a dummy image not to depend on 'horse.jpg'
         self.test_img_name = "test_dummy"
-        # Creem una imatge vermella de 100x100
+        # We create a 100x100 red image
         img = Image.new('RGB', (100, 100), color = 'red')
         img.save(f"{self.test_img_name}.jpg")
 
     def tearDown(self):
-        # Aquest mètode s'executa automàticament DESPRÉS de cada test.
-        # Esborrem els fitxers creats per deixar la carpeta neta.
+        # This method executes automatically AFTER every test
+        # We delete the created files to keep the folder clean
         if os.path.exists(f"{self.test_img_name}.jpg"):
             os.remove(f"{self.test_img_name}.jpg")
         
-        # Neteja extra dels fitxers de sortida generats pels tests
-        # (Busquem fitxers que comencin per 'test_dummy' o 'bw_compressed_test_dummy')
+        # Extra cleanance of the output files generated by the tests
+        # (We search files starting with "test_dummy" or "bw_compressed_test_dummy")
         for f in os.listdir():
             if self.test_img_name in f and f.endswith(".jpg"):
                 try:
@@ -390,28 +410,37 @@ class TestMultimediaMethods(unittest.TestCase):
 
     # --- TEST 1: ColorTranslator ---
     def test_color_translator_roundtrip(self):
+        print("\n\n========================================")
+        print("Test 2nd Task")
+        print("========================================")
         converter = ColorTranslator()
         r, g, b = 100, 150, 200
         y, u, v = converter.RGBtoYUV(r, g, b)
         r_out, g_out, b_out = converter.YUVtoRGB(y, u, v)
         
-        # Comprovem que el resultat és gairebé idèntic a l'entrada
+        # We check that the result is almost identical to the input
         self.assertAlmostEqual(r, r_out, delta=1)
         self.assertAlmostEqual(g, g_out, delta=1)
         self.assertAlmostEqual(b, b_out, delta=1)
 
     # --- TEST 2: Resize (FFmpeg) ---
     def test_resize_ffmpeg(self):
-        # Cridem a la funció amb la nostra imatge dummy
+        print("\n\n========================================")
+        print("Test 3rd Task")
+        print("========================================")
+        # Call the function witht the dummy image
         output_file = resizeImage_ffmpeg(self.test_img_name, 50, 50)
         
-        # Comprovem 1: Que la funció retorni un nom (no None)
-        self.assertIsNotNone(output_file, "La funció resize hauria de retornar el nom del fitxer")
-        # Comprovem 2: Que el fitxer realment existeixi al disc
-        self.assertTrue(os.path.exists(output_file), "El fitxer redimensionat no s'ha creat")
+        # Check 1: If the function returns a name (not None)
+        self.assertIsNotNone(output_file, "The function resize should return the file name")
+        # Check 2: If the file exists at the disk
+        self.assertTrue(os.path.exists(output_file), "The redimensioned file has not been created")
 
     # --- TEST 3: Serpentine ---
     def test_serpentine_logic(self):
+        print("\n\n========================================")
+        print("Test 4th Task")
+        print("========================================")
         # Matriu simple 3x3
         matrix = np.array([
             [1, 2, 6],
@@ -420,24 +449,33 @@ class TestMultimediaMethods(unittest.TestCase):
         ])
         expected = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         result = serpentine(matrix)
-        self.assertEqual(result, expected, "L'algorisme Serpentine no ha ordenat bé")
+        self.assertEqual(result, expected, "The Serpentine algorithm has not been ordered well")
 
     # --- TEST 4: Compression ---
     def test_compression_bw(self):
+        print("\n\n========================================")
+        print("Test 5th Task")
+        print("========================================")
         output_file = compress_bw_hardest(f"{self.test_img_name}.jpg")
         
         self.assertIsNotNone(output_file)
-        self.assertTrue(os.path.exists(output_file), "El fitxer comprimit no s'ha creat")
+        self.assertTrue(os.path.exists(output_file), "The compressed file has not been created")
         self.assertTrue(os.path.getsize(output_file) > 0)
 
     # --- TEST 5: Run Length Encoding ---
     def test_rle_logic(self):
+        print("\n\n========================================")
+        print("Test 6th Task")
+        print("========================================")
         raw_data = [10, 20, 0, 0, 0, 0, 5, 0, 0]
         expected = [10, 20, 0, 4, 5, 0, 2]
         self.assertEqual(run_length_encoding(raw_data), expected)
 
     # --- TEST 6: DCT Reversibility ---
     def test_dct_error(self):
+        print("\n\n========================================")
+        print("Test 7th Task")
+        print("========================================")
         dct = DCT_Converter(size=8)
         block = np.random.randint(0, 255, (8, 8))
         
@@ -445,11 +483,14 @@ class TestMultimediaMethods(unittest.TestCase):
         recovered = dct.perform_IDCT(coeffs)
         
         mse = np.mean((block - recovered) ** 2)
-        # Acceptem un error molt petit (per culpa dels decimals flotants)
-        self.assertLess(mse, 0.001, "L'error (MSE) de la DCT és massa gran!")
+        # We accept a very little error (due to float decimals)
+        self.assertLess(mse, 0.001, "The error (MSE) of the DCT is too big!")
 
     # --- TEST 7: DWT Reversibility ---
     def test_dwt_error(self):
+        print("\n\n========================================")
+        print("Test 8th Task")
+        print("========================================")
         dwt = DWT_Converter()
         block = np.array([
             [100, 100, 50, 50],
@@ -462,11 +503,11 @@ class TestMultimediaMethods(unittest.TestCase):
         recovered = dwt.perform_IDCT(coeffs)
         
         mse = np.mean((block - recovered) ** 2)
-        self.assertLess(mse, 0.001, "L'error (MSE) de la DWT és massa gran!")
+        self.assertLess(mse, 0.001, "The error (MSE) of the DWT is too big!")
 
 if __name__ == '__main__':
-    # Executa els tests i mostra el resultat
+    # Execute the tests and show the result
     print("\n\n========================================")
-    print("EXECUTANT TESTS UNITARIS (TASCA 8)")
+    print("EXECUTING UNITARY TESTS (9th TASK)")
     print("========================================")
     unittest.main(argv=['first-arg-is-ignored'], exit=False)
